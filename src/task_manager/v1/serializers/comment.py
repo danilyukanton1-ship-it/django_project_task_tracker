c068ed2d2e486316bf60b0a356fd62f6bad6eac6
Anton Danilyuk <danilyuk.anton2023@mail.ru>
2026-04-22 20:19:11 +0300

    add nested serializers for comment.user and comment.task

--- src/task_manager/v1/serializers/comment.py
+++ src/task_manager/v1/serializers/comment.py
@@ -2,7 +2,23 @@ from rest_framework import serializers
 from task_manager.models import Comments
 
 
+class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    username = serializers.CharField()
+    email = serializers.EmailField()
+
+
+class TaskSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+    priority = serializers.IntegerField()
+    status = serializers.CharField()
+
+
 class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    task = TaskSerializer(read_only=True)
+
     class Meta:
         model = Comments
         fields = ("id", "message", "user", "task")
