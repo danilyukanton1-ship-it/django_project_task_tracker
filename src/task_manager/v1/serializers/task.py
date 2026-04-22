8694e9272e04b6b3b1d2cd36c21424a3fb60af7f
Anton Danilyuk <danilyuk.anton2023@mail.ru>
2026-04-22 20:02:33 +0300

    fix dependence of task serializer on comment serializer

--- src/task_manager/v1/serializers/task.py
+++ src/task_manager/v1/serializers/task.py
@@ -1,40 +1,29 @@
 from rest_framework import serializers
 from task_manager.models import Tasks, Projects
 from account.models import User
-from task_manager.v1.serializers.comment import CommentSerializer
-from django.core.cache import caches
-from django.contrib.auth import get_user_model
-
-# class TaskSerializer(serializers.Serializer):
-#     id = serializers.IntegerField(read_only=True)
-#     name = serializers.CharField(required=True, allow_blank=False, max_length=100)
-#     description = serializers.CharField(required=False, allow_blank=True, max_length=255)
-#     priority = serializers.IntegerField()
-#
-#     def create(self, validated_data):
-#         """
-#         Create and return a new `Snippet` instance, given the validated data.
-#         """
-#         return Tasks.objects.create(**validated_data)
-#
-#     def update(self, instance, validated_data):
-#         """
-#         Update and return an existing `Snippet` instance, given the validated data.
-#         """
-#         instance.name = validated_data.get("name", instance.name)
-#         instance.description = validated_data.get("description", instance.description)
-#         instance.priority = validated_data.get("priority", instance.priority)
-#         instance.save()
-#         return instance
-#
 
 
 class UserTasksSerializer(serializers.Serializer):
+
     id = serializers.IntegerField()
     username = serializers.CharField()
     first_name = serializers.CharField()
     last_name = serializers.CharField()
     email = serializers.EmailField()
+
+    def __init__(self, *args, **kwargs):
+        """Если нужны не все поля прописываем all_fields=False и выводим только id, username и email"""
+        all_fields = kwargs.pop("all_fields", True)
+        super().__init__(*args, **kwargs)
+        if not all_fields:
+            self.fields.pop("first_name", None)
+            self.fields.pop("last_name", None)
+
+
+class CommentSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    message = serializers.CharField()
+    user = UserTasksSerializer(read_only=True, all_fields=False)
 
 
 class TaskSerializer(serializers.ModelSerializer):
