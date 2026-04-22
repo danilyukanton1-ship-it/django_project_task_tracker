73e0cda58bf3aac4ea2a01368b828c4c2f1bb220
Anton Danilyuk <danilyuk.anton2023@mail.ru>
2026-04-22 20:21:17 +0300

    add nested serializer for project.owner

--- src/task_manager/v1/serializers/project.py
+++ src/task_manager/v1/serializers/project.py
@@ -1,8 +1,18 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from task_manager.models import Projects
 
 
-class ProjectSerializer(ModelSerializer):
+class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    username = serializers.CharField()
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    email = serializers.EmailField()
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    owner = UserSerializer(read_only=True)
+
     class Meta:
         model = Projects
         fields = ("id", "name", "description", "owner")
