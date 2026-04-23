155ccf60b94fdcb0df1d0d432c07bd3b5a8eac17
Anton Danilyuk <danilyuk.anton2023@mail.ru>
2026-04-23 19:37:47 +0300

    add djangorestframework_simplejwt to reqs

--- src/account/v1/views/user.py
+++ src/account/v1/views/user.py
@@ -1,9 +1,5 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from account.models import User
 from account.v1.serializers import UserSerializer
-from rest_framework import status
-from django.http import Http404
 from rest_framework.viewsets import ModelViewSet
 from drf_spectacular.utils import extend_schema
 
