1c08c5767b82fe148ccd77fbe0aa33ce5b73165c
Anton Danilyuk <danilyuk.anton2023@mail.ru>
2026-05-02 22:57:12 +0300

    delete custom filter for request.user

--- src/task_manager/v1/views/task.py
+++ src/task_manager/v1/views/task.py
@@ -1,9 +1,6 @@
 from task_manager.models import Tasks, Comments
 from task_manager.v1.serializers import TaskSerializer
-from task_manager.v1.filters import (
-    TaskQueryFilterSerializer,
-    TaskRequestUserFilterSerializer,
-)
+from task_manager.v1.filters import TaskQueryFilterSerializer
 from rest_framework.response import Response
 from drf_spectacular.utils import extend_schema
 from django.core.cache import caches
@@ -24,7 +21,7 @@ class TaskAPIViewSet(ModelViewSet):
     )
     serializer_class = TaskSerializer
     pagination_class = CustomPagination
-    filterset_class = (TaskQueryFilterSerializer, TaskRequestUserFilterSerializer)
+    filterset_class = (TaskQueryFilterSerializer,)
 
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
