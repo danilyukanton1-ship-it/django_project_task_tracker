3fa7fce0615b1da387bd5fb71848451af33155fd
Anton Danilyuk <danilyuk.anton2023@mail.ru>
2026-05-02 19:55:47 +0300

    Adding custom pagination with 10 users per page in UsersAPI, and creating DynamicPageSizePagination class

--- src/account/v1/views/user.py
+++ src/account/v1/views/user.py
@@ -2,9 +2,12 @@ from account.models import User
 from account.v1.serializers import UserSerializer
 from rest_framework.viewsets import ModelViewSet
 from drf_spectacular.utils import extend_schema
+from config.pagination import CustomPagination
 
 
 @extend_schema(tags=["User"])
 class UserAPIViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    pagination_class = CustomPagination
+    pagination_serializer_class = UserSerializer
--- src/config/pagination.py
+++ src/config/pagination.py
@@ -5,7 +5,23 @@ from rest_framework.response import Response
 
 
 class CustomPagination(PageNumberPagination):
-    page_size = 20
+    page_size = 10
+
+    def get_paginated_response(self, data):
+        return Response(
+            {
+                "count": self.page.paginator.count,
+                "count_pages": ceil(self.page.paginator.count // self.page_size),
+                "page": self.page.number,
+                "results": data,
+            }
+        )
+
+
+class DynamicPageSizePagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = "page_size"
+    max_page_size = 50
 
     def get_paginated_response(self, data):
         return Response(
